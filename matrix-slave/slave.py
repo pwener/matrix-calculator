13af18872c97cec493fb96eef2abd7e653e038bc
pwener <phwener@gmail.com>
2017-05-31 12:06:34 -0300

    Slave using pair_in

--- matrix-slave/slave.py
+++ matrix-slave/slave.py
@@ -4,6 +4,8 @@ import simplejson as json
 import ast
 import sys
 
+REPOSITORY_URL = "http://localhost:3000/repository"
+
 def multiply(A, B):
     # The new line have this size too
     size = len(A)
@@ -21,6 +23,10 @@ def multiply(A, B):
 def set_to_array(x):
     return x.replace('{', '[').replace('}', ']')
 
+def repo_pair_in(key, value):
+    # repr converts code to string
+    r = requests.post(REPOSITORY_URL + '/pair_in/' + key + '/' + value)
+
 if __name__ == '__main__':
     order = int(sys.argv[1])
 
@@ -28,9 +34,9 @@ if __name__ == '__main__':
 
     for i in range(0, order):
         for j in range(0, order):
+            key = str(i) + 'x' + str(j)
             try:
-                key = str(i) + 'x' + str(j)
-                get_task = urllib.request.urlopen("http://localhost:3000/repository/pair_out/"+key)
+                get_task = urllib.request.urlopen(REPOSITORY_URL+"/pair_out/"+key)
                 task_json = get_task.read()
                 task_dir = json.loads(task_json)
             except urllib.error.HTTPError:
@@ -46,6 +52,8 @@ if __name__ == '__main__':
             A = eval(set_to_array(matrix_a['content']))
             B = eval(set_to_array(matrix_b['content']))
 
-            R = multiply(A, B)
+            result = multiply(A, B)
 
-            print(R)
+            print("Result is " + str(result))
+
+            repo_pair_in(key, str(result))
