7ad64095a3909e0e1f13ef49900ce9c06f752c1a
pwener <phwener@gmail.com>
2017-05-31 00:19:30 -0300

    Adding master to post one matrix by request

--- matrix-master/master.py
+++ matrix-master/master.py
@@ -32,14 +32,16 @@ def to_hash(arr):
     return final_hash
 
 
-# Create pairs to multiply AxB
-def create_pairs_with(A, B):
-    # B[0] or A is the order
-    order = len(B[0])
 
+def create_pairs_with_lines(A):
     for index, line in enumerate(A):
         # set converts [] to {}
         repo_request('A'+str(index), to_hash(line))
+
+
+def create_pairs_with_columns(B):
+    # B[0] or A is the order
+    order = len(B[0])
 
     for index in range(order):
         column = [c[index] for c in B]
@@ -49,15 +51,17 @@ def create_pairs_with(A, B):
 if __name__ == '__main__':
     try:
         # Name of matrix A and B
-        filenameA = sys.argv[1]
-        filenameB = sys.argv[2]
+        filename = sys.argv[1]
+        option = sys.argv[2]
 
-        A = read_matrix(filenameA)
-        B = read_matrix(filenameB)
+        matrix = read_matrix(filename)
 
-        create_pairs_with(A, B)
-
-        R = [[0 for x in range(len(A))] for y in range(len(B[0]))]
+        if option == 'A':
+            create_pairs_with_lines(matrix)
+        elif option == 'B':
+            create_pairs_with_columns(matrix)
+        else:
+            print("Invalid, type A or B has second argument...")
 
     except ValueError:
-        print("Ops! You need run: python3 master.py <filenameA> <filenameB>")
+        print("Ops! You need run: python3 master.py <filenameA> <A or B>")
